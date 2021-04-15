ec7b0e08d901019b8c3dd92d3748fbf62d1016f7
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-04-15 20:31:42 +0530

    Update EmployeeControllerTest.java

--- src/test/java/com/javatraining/jpaexample/controller/EmployeeControllerTest.java
+++ src/test/java/com/javatraining/jpaexample/controller/EmployeeControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javatraining.jpaexample.JpaExampleApplication;
 import com.javatraining.jpaexample.jpa.entity.Employee;
@@ -30,6 +31,7 @@ public class EmployeeControllerTest {
 
     @MockBean
     EmployeeService employeeService;
+
 
     @Test
     public void testGetAllEmployees() throws Exception {
@@ -71,30 +73,10 @@ public class EmployeeControllerTest {
 
     }
 
-    /*
-     * @Test public void testDeleteEmployee(Integer id) throws Exception { Employee e1 = new
-     * Employee(); e1.setEmpid(1);
-     * 
-     * 
-     * // when(employeeService.deleteEmployee(1)).thenReturn(e1);
-     * 
-     * MvcResult res = mockMvc.perform(get("/deleteEmployee").accept(MediaType.APPLICATION_JSON))
-     * .andExpect(status().isOk()) .andReturn(); Employee e = new
-     * ObjectMapper().readValue(res.getResponse() .getContentAsString(), Employee.class);
-     * assertEquals(e.getEmpid(), 1); }
-     * 
-     * @Test public void testsaveEmployee(Integer id) throws Exception { Employee e1 = new
-     * Employee(); e1.setEmpid(1);
-     * 
-     * 
-     * // when(employeeService.deleteEmployee(1)).thenReturn(e1);
-     * 
-     * MvcResult res = mockMvc.perform(get("/saveEmployee").accept(MediaType.APPLICATION_JSON))
-     * .andExpect(status().isOk()) .andReturn(); Employee e = new
-     * ObjectMapper().readValue(res.getResponse() .getContentAsString(), Employee.class);
-     * assertEquals(e.getEmpid(), 1);
-     * 
-     * }
-     */
+    @Test
+    public void testdeleteEmployee() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/deleteEmployee/1"))
+                .andExpect(status().isOk());
+    }
 
 }
