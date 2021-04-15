f257939c07d1e2f1d1e4bbd5072d4f773aa22592
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-04-15 20:35:41 +0530

    Update JpaExampleApplicationTests.java

--- src/test/java/com/javatraining/jpaexample/JpaExampleApplicationTests.java
+++ src/test/java/com/javatraining/jpaexample/JpaExampleApplicationTests.java
@@ -2,12 +2,13 @@ package com.javatraining.jpaexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-class JpaExampleApplicationTests {
+@ContextConfiguration(classes = {JpaExampleApplication.class})
+public class JpaExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
 }
