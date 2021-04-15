1a036d4cb994822173b94e6d2949845e2880fdee
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-04-15 12:28:31 +0530

    Update Controller.java

--- src/main/java/com/javatraining/jpaexample/controller/EmployeeController.java
+++ src/main/java/com/javatraining/jpaexample/controller/EmployeeController.java
@@ -1,11 +1,18 @@
 package com.javatraining.jpaexample.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.javatraining.jpaexample.jpa.entity.Employee;
@@ -29,14 +36,30 @@ public class EmployeeController {
         return new ResponseEntity(employeeService.getEmployeeById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/deleteEmployee")
-    public void deleteEmployee(@RequestParam("id") Integer id) {
-        new ResponseEntity(HttpStatus.OK);
+    @DeleteMapping("/deleteEmployee")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Integer id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        employeeService.deleteEmployee(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
-    @GetMapping("/saveEmployee")
-    public void saveEmployee(@RequestParam("id") Integer id) {
-        new ResponseEntity(HttpStatus.OK);
+
+    @PostMapping("/createEmployee")
+    public void createEmployee(@Valid @RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
     }
 
+    @PutMapping("/updateEmployee")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Integer id, @Valid @RequestBody Employee employeeDetails) {
+        Employee employee = employeeService.getEmployeeById(id);
+
+
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setName(employeeDetails.getName());
+        employee.setPhone(employeeDetails.getPhone());
+        final Employee updatedEmployee = employeeService.createEmployee(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
